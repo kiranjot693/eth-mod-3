03a032ab9361e204b29c74bf5d60258253337660
Kiran Jot Singh <167282873+kiranjot693@users.noreply.github.com>
2024-07-23 14:30:42 +0530

    Update kirantoken.sol

--- kirantoken.sol
+++ kirantoken.sol
@@ -1,1 +1,34 @@
+pragma solidity ^0.8.18;
 
+contract kirantoken{
+    string public tokenName = "kiran";
+    string public tokenAbbr = "KJT";
+
+    address owner;
+
+    mapping(address => uint) record;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function getBalance() external view returns (uint){
+        return record[msg.sender];
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == owner,"Only owner can mint tokens");
+        record[to] += amount;
+    }
+
+    function transferTo(address to, uint amount) external{
+        require(record[msg.sender] >= amount,"Insufficient balance ");
+        record[to] += amount;
+        record[msg.sender] -= amount;
+    }
+
+    function burn(uint amount) external {
+        require(record[msg.sender] >= amount,"Insufficient balance");
+        record[msg.sender] -= amount;
+    }
+}
